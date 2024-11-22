76c2c99663390eee3ea9f0900ec07940a99f8a99
thunderwin <woooms@qq.com>
2024-11-22 20:55:39 +0800

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -10,8 +10,7 @@ module.exports = defineConfig({
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-  
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret"
     },
     admin:{
       disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
