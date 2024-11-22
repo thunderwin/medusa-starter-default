61272c2ed0bb57afc8eb4924d4f9eb7dbd88e5e2
thunderwin <woooms@qq.com>
2024-11-22 22:07:30 +0800

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -4,17 +4,17 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
-    http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret"
-    },
-    admin:{
-      disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
-      backendUrl: process.env.MEDUSA_BACKEND_URL,
-      path: process.env.MEDUSA_ADMIN_PATH,
-    }
+    database_url: process.env.DATABASE_URL,
+    store_cors: process.env.STORE_CORS!,
+    admin_cors: process.env.ADMIN_CORS!,
+    auth_cors: process.env.AUTH_CORS!,
+    jwt_secret: process.env.JWT_SECRET || "supersecret",
+    cookie_secret: process.env.COOKIE_SECRET || "supersecret",
+  },
+  // admin 配置应该是顶级配置
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    backendUrl: process.env.MEDUSA_BACKEND_URL,
+    path: process.env.MEDUSA_ADMIN_PATH,
+  }
 })
